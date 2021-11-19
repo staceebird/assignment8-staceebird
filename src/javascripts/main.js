d5a30bb30d9f3afc7018bdaa5c5a9279b8c288a5
Stacee Bird <staceebird@mail.weber.edu>
2021-11-18 17:48:33 -0700

    'done'

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,3 +4,5 @@ require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 //TODO - Your ES6 JavaScript code (if any) goes here
+import 'bootstrap'
+import * as fontawesome from "@fortawesome/fontawesome-free/js/all"
